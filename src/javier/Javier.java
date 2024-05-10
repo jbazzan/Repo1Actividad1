36997a7e6206fe4efbfdc98c651bfe1502b8a43d
Javier Emiliano Bazzan <75402275+jbazzan@users.noreply.github.com>
2024-05-10 19:18:52 +0200

    Creacion de clases y metodos para el proyecto

---
+++ src/javier/Javier.java
@@ -0,0 +1,12 @@
+package javier;
+
+public class Javier {
+	
+	private String nombre = "Javier";
+	
+	public void nombre(){
+		System.out.println("Hola, Soy "+ nombre);
+		return;
+	}
+	
+}
---
+++ src/main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+import javier.Javier;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Javier javier = new Javier();
+		javier.nombre();
+	
+	}
+}
